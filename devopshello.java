13e6215501e883208d9a36c962eb551c4fa3e83f
Gokulkrishna2210 <2020wa86825@wilp.bits-pilani.ac.in>
2024-06-06 18:55:34 +0000

    Update devopshello.java

--- devopshello.java
+++ devopshello.java
@@ -1,8 +1,6 @@
 /** The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.**/
-public class devopshello
-{
-public static void main(String[] args) 
-  {
+public class devopsHello{
+public static void main(String[] args) {
 System.out.println("Welcome to DevOps,");
-  }
+}
 }
